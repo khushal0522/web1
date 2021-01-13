147f81e0c397d88d341fc453d79e0ddf7ddff211
khushal0522 <55732831+khushal0522@users.noreply.github.com>
2021-01-13 15:19:37 +0530

    Add files via upload

---
+++ build/precache-manifest.8b4e3fa6a436a63c7279512b58f9daaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fec416bea6a11e4cc43197a53a04771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b586384dd90cc52ba95c",
+    "url": "/static/css/main.83ac07f8.chunk.css"
+  },
+  {
+    "revision": "6c1bc929c60d492ca6b6",
+    "url": "/static/js/2.dcc322de.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.dcc322de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b586384dd90cc52ba95c",
+    "url": "/static/js/main.0a9e9e81.chunk.js"
+  },
+  {
+    "revision": "cc88bcdb6b0ae34a6f4f",
+    "url": "/static/js/runtime-main.ec40590b.js"
+  }
+]);
